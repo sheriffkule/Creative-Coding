7619fd443b5a46167b2d6f8e4ab16615d4d90794
SheriffKule <zeljko225@yahoo.com>
2025-12-05 11:43:31 +0100

    Animating Voice & Music

---
+++ 021 - Animating Voice & Music/visualizer.js
@@ -0,0 +1,35 @@
+function main() {
+  const canvas = document.getElementById('myCanvas');
+  const ctx = canvas.getContext('2d');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  class Bar {
+    constructor(x, y, width, height, color) {
+      this.x = x;
+      this.y = y;
+      this.width = width;
+      this.height = height;
+      this.color = color;
+    }
+    update(micInput) {
+      // this.height = micInput
+      this.x++;
+    }
+    draw(context) {
+      context.fillStyle = this.color;
+      context.fillRect(this.x, this.y, this.width, this.height);
+    }
+  }
+
+  const bar1 = new Bar(10, 10, 100, 200, 'blue');
+
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    bar1.draw(ctx);
+    // generates audio samples from microphone
+    // animate bars based on microphone data
+    requestAnimationFrame(animate);
+  }
+  animate();
+}
